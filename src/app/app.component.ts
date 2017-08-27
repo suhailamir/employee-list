272717ffe8a283945f4d723ad0d417365b94f35a
muhammad <suhail.amir101@gmail..com>
2017-08-27 16:36:18 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import {Component} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
+export class AppComponent {
+  employees = [
+    {
+      id: '123',
+      title: 'John',
+      bio: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with the release of 
+        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+        software like Aldus PageMaker including versions of Lorem Ipsum.`,
+      opened: false
+    }, {
+      id: '1234',
+      title: 'Adam',
+      bio: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with the release of 
+        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+        software like Aldus PageMaker including versions of Lorem Ipsum.`,
+      opened: false
+    }, {
+      id: '1134',
+      title: 'Steve',
+      bio: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with the release of 
+        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+        software like Aldus PageMaker including versions of Lorem Ipsum.`,
+      opened: false
+    }
+  ];
+
+  modalData = {};
+
+  constructor(private modal : NgbModal) {}
+
+  toggle(employee) {
+    this.employees = this
+      .employees
+      .map((x) => {
+        if (x.id === employee.id) {
+          employee.opened = !employee.opened;
+          return employee;
+        } else {
+          x.opened = false;
+          return x;
+
+        }
+      })
+
+  }
+  openModal(modalContent, employee) {
+    this.modalData['employee'] = employee;
+    this
+      .modal
+      .open(modalContent, {size: 'lg'})
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {AppComponent} from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule, FormsModule, HttpModule, NgbModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
